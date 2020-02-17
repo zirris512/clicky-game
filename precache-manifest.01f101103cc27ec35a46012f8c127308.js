2042d697b6d2f8f9d7b45f2daf2b4ec4378dbedf
Brent <mina512@live.com>
2020-02-16 23:46:54 -0600

    Updates

---
+++ precache-manifest.01f101103cc27ec35a46012f8c127308.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb6ecc5a2212f56bd77c6ec48ac08bbc",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "d40899f9ed1c7b9a4f48",
+    "url": "/clicky-game/static/css/main.fa00e77f.chunk.css"
+  },
+  {
+    "revision": "e10bd52dee950252efd7",
+    "url": "/clicky-game/static/js/2.8be420aa.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky-game/static/js/2.8be420aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d40899f9ed1c7b9a4f48",
+    "url": "/clicky-game/static/js/main.999518b5.chunk.js"
+  },
+  {
+    "revision": "a444cfe878aa3a515eec",
+    "url": "/clicky-game/static/js/runtime-main.24c47d54.js"
+  },
+  {
+    "revision": "24aef95c9d5fce508b4bae73af8f70c9",
+    "url": "/clicky-game/static/media/dekuMM.24aef95c.png"
+  },
+  {
+    "revision": "d950da0f204dff68dd5f54b04ff76379",
+    "url": "/clicky-game/static/media/ganon.d950da0f.png"
+  },
+  {
+    "revision": "4cdb95306b117e78e67e0111c6426d34",
+    "url": "/clicky-game/static/media/ganonOoT.4cdb9530.jpg"
+  },
+  {
+    "revision": "ccb1783b250776774986cb9028980b0c",
+    "url": "/clicky-game/static/media/linkBotW.ccb1783b.png"
+  },
+  {
+    "revision": "bd1de4ea88c4faa85ccfe2ac435963c5",
+    "url": "/clicky-game/static/media/linkWW.bd1de4ea.png"
+  },
+  {
+    "revision": "9b574f42aab2d9ff16e16210646e4694",
+    "url": "/clicky-game/static/media/majora.9b574f42.png"
+  },
+  {
+    "revision": "fbf3ac93702216b791b43899cd9a675b",
+    "url": "/clicky-game/static/media/sheik.fbf3ac93.jpg"
+  },
+  {
+    "revision": "c0823c14dd4c34de7e7ea20f24ecbedf",
+    "url": "/clicky-game/static/media/triforce.c0823c14.png"
+  },
+  {
+    "revision": "e4f47fdc0a0eda78dc38ba702ddcc9e4",
+    "url": "/clicky-game/static/media/zeldaBotW.e4f47fdc.png"
+  }
+]);
