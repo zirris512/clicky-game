0b3186a5d124c9dc4e7386742b1624de5115fe54
Brent <mina512@live.com>
2020-05-02 16:05:23 -0500

    Fix linting

--- clicky-game/src/utils/Context.js
+++ clicky-game/src/utils/Context.js
@@ -12,27 +12,46 @@ const initialState = {
 };
 
 const reducer = (state, action) => {
-    switch (action.type) {
-        case "notClicked":
-            if (state.score === state.total) {
-                return {...state, score: state.score + 1, total: state.total + 1, clicked: [...state.clicked, action.value], images: action.shuffle};
-            }
-            return {...state, score: state.score + 1, clicked: [...state.clicked, action.value], images: action.shuffle}
-        case "clicked":
-            return {...state, score: initialState.score, clicked: initialState.clicked, images: action.shuffle}
-        default:
-            throw new Error(`Invalid action type: ${action.type}`);
-    }
+   switch (action.type) {
+      case "notClicked":
+         if (state.score === state.total) {
+            return {
+               score: state.score + 1,
+               total: state.total + 1,
+               clicked: [
+                  ...state.clicked,
+                  action.value],
+               images: action.shuffle
+            };
+         }
+         return {
+            ...state,
+            score: state.score + 1,
+            clicked: [
+               ...state.clicked,
+               action.value
+            ],
+            images: action.shuffle
+         }
+      case "clicked":
+         return {
+            ...state,
+            score: initialState.score,
+            clicked: initialState.clicked,
+            images: action.shuffle}
+      default:
+         throw new Error(`Invalid action type: ${action.type}`);
+   }
 };
 
 const ScoreProvider = ({ ...props }) => {
-    const [state, dispatch] = useReducer(reducer, initialState);
+   const [state, dispatch] = useReducer(reducer, initialState);
 
-    return <Provider value={[state, dispatch]} {...props} />;
+   return <Provider value={[state, dispatch]} {...props} />;
 };
 
 const useScoreContext = () => {
-    return useContext(ScoreContext);
+   return useContext(ScoreContext);
 };
 
 export { ScoreProvider, useScoreContext }
